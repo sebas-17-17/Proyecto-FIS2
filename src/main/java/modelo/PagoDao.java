5723a67587da46de22761939a1c19dc56c59bc8b
Alan Jami <atlas5we3ty@gmail.com>
2026-02-05 03:21:21 -0500

    Eliminacion de codigo innecesario

--- src/main/java/modelo/PagoDao.java
+++ src/main/java/modelo/PagoDao.java
@@ -7,11 +7,9 @@ import java.io.IOException;
 
 public class PagoDao {
     private File archivoTarjetas;
-    private File archivoPagos;
     
     public PagoDao() {
         archivoTarjetas = new File("Tarjetas.txt");
-        archivoPagos = new File("pago.txt");
     }
     // Guarda los datos de la tarjeta ingresada, en el archivo Tarjetas.txt
     public boolean guardarPago(Pago p) {
@@ -24,23 +22,5 @@ public class PagoDao {
             event.printStackTrace();
             return false;
         }
-    }
-    /*
-    // TODO: Actualizar este metodo cuando tengamos los setters y getters
-    // Guarda el resumen del pago
-    public boolean guardarPagoResumen(String cedula, String metodo, double total) {
-        try (FileWriter fw = new FileWriter(archivoPagos, true);
-             BufferedWriter bw = new BufferedWriter(fw)) {
-
-            bw.write(cedula + "," + metodo + "," + total);
-            bw.newLine();
-            return true;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    */
-    
+    } 
 }
